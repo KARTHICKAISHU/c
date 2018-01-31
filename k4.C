75951e8f15ab1a7280cc203928f7ae9a561fa193
KARTHICKAISHU <35991674+KARTHICKAISHU@users.noreply.github.com>
2018-01-31 15:04:33 +0530

    Update k4.C

--- k4.C
+++ k4.C
@@ -1,15 +1,13 @@
-#include <stdio.h>
- 
-int main()
+#include<stdio.h>
+int main ()
 {
-  char ch;
-  printf("\n Please Enter any character \n");
-  scanf("%c", &ch);
-  
-  if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') )
-    printf("\n %c is an Alphabet", ch);
-  else
-    printf("\n %c is not an Alphabet", ch);
-  
-  return 0;
-}
+ char  ch;
+ printf("\n please enter any character \n");
+ scanf("%c",&ch);
+ if((ch>='a' && ch<='z'||(ch>='A' && ch<='Z'))
+    printf("\n %c is an alphabet", ch);
+    else 
+    printf("\n%c is not an alphabet", ch);
+    return 0;
+    }
+   
